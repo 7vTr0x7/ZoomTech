7d3cb2b48e2459aaa3d309c82a5a08c04f1b2fad
7vTr0x7 <7vTrox7@gmail.com>
2024-12-17 12:25:05 +0530

    Services

--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaArrowRight } from "react-icons/fa"; // Import right arrow icon
+import { GoArrowRight } from "react-icons/go";
 import dribbleShot from "../../assets/images/Dribbbleshot.png";
 
 const ServiceList = () => {
@@ -26,30 +26,25 @@ const ServiceList = () => {
           className={`mb-10 flex flex-col sm:flex-row gap-5 items-center justify-center mx-auto ${
             index % 2 === 0 ? "sm:flex-row" : "sm:flex-row-reverse"
           }`}>
-          {/* Image Section */}
           <div className="flex items-center justify-center sm:w-1/2 overflow-hidden">
             <div className="relative w-full max-w-[300px] sm:max-w-[400px]">
               <img
                 alt={`${service.service} image`}
                 src={dribbleShot}
-                className="w-full h-72 sm:h-96 object-cover rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
+                className="w-full h-72 sm:h-96 object-cover rounded-lg  transition-transform duration-300 hover:scale-105"
               />
               <div
-                className="bg-[#F5B22B] absolute bottom-0 left-0 flex items-center justify-center rounded-full transition-all duration-300 transform hover:scale-110"
-                style={{
-                  width: "120px",
-                  height: "120px",
-                  marginLeft: "-20px",
-                  marginBottom: "0px",
-                }}>
-                <p className="text-black font-semibold text-sm text-center">
+                className="bg-[#F5B22B] absolute bottom-0 left-0 flex items-center justify-center rounded-full transition-all duration-300 transform hover:scale-110
+             w-[80px] h-[80px] ml-[0px] mb-[0px]
+             sm:w-[100px] sm:h-[100px] sm:ml-[0px]
+             md:w-[120px] md:h-[120px] md:ml-[-20px] md:mb-[0px]">
+                <p className="text-black font-semibold text-xs sm:text-sm text-center">
                   View Projects
                 </p>
               </div>
             </div>
           </div>
 
-          {/* Service Description Section */}
           <div className="flex flex-col sm:w-1/2 text-center sm:text-left">
             <h3 className="text-3xl sm:text-5xl font-semibold text-gray-800 mb-3">
               {service.service}
@@ -61,10 +56,10 @@ const ServiceList = () => {
               inceptos himenaeos. Class aptent taciti sociosqu ad litora
               torquent per conubia nostra, per inceptos himenaeos.
             </p>
-            {/* View Details Button with Right Arrow */}
-            <button className="inline-flex items-center text-[#F5B22B] font-semibold hover:text-[#C5911A] transition-all duration-300">
-              View Details
-              <FaArrowRight className="ml-2 text-xl" />
+
+            <button className="inline-flex items-center justify-center mt-4 md:mt-6 bg-black text-white py-2 px-4 rounded-3xl w-fit mx-auto md:mx-0">
+              <span className="mr-6 font-medium">View Details</span>
+              <GoArrowRight className="text-xl" />
             </button>
           </div>
         </div>
